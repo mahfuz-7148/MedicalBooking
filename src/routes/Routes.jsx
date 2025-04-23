2b2beb5d94133905f862a9bd23470b092802df2c
mahfuz-7148 <mrahman7148@gmail.com>
2025-04-23 13:39:44 +0600

    initial

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,7 @@ import Contact from "../pages/Contact.jsx";
 import BookingDetails from "../pages/BookingDetails.jsx";
 import Appoinment from "../components/Appoinment.jsx";
 import Error from "../pages/Error.jsx";
+import ErrorDetails from "../components/ErrorDetails.jsx";
 
 const router = createBrowserRouter([
     {
@@ -36,6 +37,10 @@ const router = createBrowserRouter([
                 path: '/doctor/:id',
                 Component: BookingDetails,
                 loader:() => fetch('/fakeData.json')
+            },
+            {
+                path: '*',
+                Component: ErrorDetails
             }
         ]
     }
