ed08848dcb743e96392b9158ef73bb6495a068ae
mahfuz-7148 <mrahman7148@gmail.com>
2025-04-23 22:05:45 +0600

    updated

--- src/pages/BookingDetails.jsx
+++ src/pages/BookingDetails.jsx
@@ -73,7 +73,7 @@ const doctorDetail = data.find(doctor => doctor.registrationNumber === id)
                     <h1 className='badge badge-outline text-[#09982F] text-lg bg-[#176AE51A] p-4 font-semibold rounded-full'>Doctor Available Today</h1>
                 </div>
                     <p className='border-b border-solid mb-5 border-[#0F0F0F33]'></p>
-                    <h1 className='md:badge md:badge-outline text-[#FFA000] text-lg bg-[#FFA0001A] md:p-4 font-semibold rounded-lg md:rounded-full'><i class="ri-error-warning-line "></i> Due to high patient volume, we are currently accepting appointments for today only. We appreciate your understanding and cooperation.</h1>
+                    <h1 className='text-[#FFA000] text-lg bg-[#FFA0001A] p-2 px-10 font-semibold rounded-lg md:rounded-full'><i class="ri-error-warning-line "></i> Due to high patient volume, we are currently accepting appointments for today only. We appreciate your understanding and cooperation.</h1>
 
                 <Link to='/bookings'>
                     <button onClick={() => addBookings(doctorDetail)} className="w-full mt-10 relative cursor-pointer rounded-4xl px-5 py-2.5 overflow-hidden border border-blue-700 group bg-white relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-black hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
